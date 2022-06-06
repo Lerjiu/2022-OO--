59080ecd5c8291f9b7df05437a74f22557f61858
takune <1525124406@qq.com>
2022-06-06 15:39:59 +0800

    完成登录和注册功能

---
+++ src/com/JavaPro/dao/userDao.java
@@ -0,0 +1,96 @@
+package com.JavaPro.dao;
+
+import com.JavaPro.model.user;
+import com.JavaPro.util.DbUtil;
+
+import java.sql.*;
+import java.text.MessageFormat;
+
+/**
+ * 用户Dao类
+ * @author 周文瑞 20373804
+ */
+public class userDao {
+    /**
+     * 用户登录函数
+     * @param con 连接数据库的Connection类
+     * @param u     用户类
+     * @return resultUser 用户类
+     * @throws Exception
+     */
+    public user login(Connection con,user u)throws Exception{
+        user resultUser = null;
+        String sql = "SELECT * FROM user WHERE uname = ? AND passwd = ?";
+        PreparedStatement pstmt =  con.prepareStatement(sql);
+        pstmt.setString(1,u.getUserName()); //设置问号1内容
+        pstmt.setString(2,u.getPassword()); //设置问号2内容
+        ResultSet rs =  pstmt.executeQuery();   //执行sql语句,返回一个结果集
+        if(rs.next()) {
+            resultUser = new user();
+            resultUser.setUid(rs.getInt("uid"));
+            resultUser.setUserName(rs.getString("uname"));
+            resultUser.setPassword(rs.getString("passwd"));
+            System.out.println("登录成功!");
+        }
+        pstmt.close();
+        return resultUser;
+    }
+
+    /**
+     * 用户注册函数
+     * @param con 连接数据库的Connection类
+     * @param u 用户类
+     * @return
+     * @throws Exception
+     */
+    public int register (Connection con,user u)throws Exception{
+        String sql = "INSERT INTO user(uname,passwd) VALUES(?,?)";
+        String UID;
+        int result;
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1,u.getUserName());
+        pstmt.setString(2,u.getPassword());
+        result = pstmt.executeUpdate();
+        pstmt.close();
+        //插入新用户↑
+        if( result == 0 )   return result;
+        //是否插入成功↑
+        String sql1 = "SELECT * FROM user WHERE uname = ?";
+        PreparedStatement pstmt1 = con.prepareStatement(sql1);
+        pstmt1.setString(1,u.getUserName());
+        ResultSet rs1 = pstmt1.executeQuery();
+        rs1.next();
+        UID = Integer.toString(rs1.getInt("uid"));
+        pstmt1.close();
+        //得到该用户的uid用来创建表↑
+        String sql2 = "CREATE TABLE {0}" +
+                "(fName   varchar(255) not null," +
+                "content blob         not null," +
+                "primary key (fName))";
+        String Fname = "file_"+UID;
+        Statement statement = con.createStatement();
+        statement.execute(MessageFormat.format(sql2,Fname));
+        statement.close();
+        //创建新表
+        System.out.println("成功");
+        return result;
+    }
+
+/* 这是一个测试方法
+    public static void main(String[] args){
+        user u1 = new user();
+        u1.setUserName("王起");
+        u1.setPassword("123125");
+        try {
+            Connection con = DbUtil.getConnection();
+            userDao userdao = new userDao();
+            //userdao.register(con,u1);
+            userdao.login(con,u1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+ */
+}
--- src/com/JavaPro/util/DbUtil.java
+++ src/com/JavaPro/util/DbUtil.java
@@ -1,54 +1,86 @@
 package com.JavaPro.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /**
- * 数据库工具类
+ * @describe 数据库工具类
  * @author 周文瑞 20373804
  */
 public class DbUtil {
-    private String dbUrl = "jdbc:mysql://43.138.40.55:3306/JavaPro"; //数据库连接地址
-    private String dbUsername = "javauser"; //数据库用户名字
-    private String dbPassWord = "123456";   //数据库用户密码
-    private String jdbcName = "com.mysql.jdbc.Driver";  //mysql驱动;
-
     /**
-     * 连接数据库
+     * @describe  连接数据库
      * @return con
      * @throws Exception
      */
-    public Connection getCon()throws Exception{
+    public static Connection getConnection()throws Exception{
+
+        String dbUrl = "jdbc:mysql://43.138.40.55:3306/JavaPro"; //数据库连接地址
+        String dbUsername = "javauser"; //数据库用户名字
+        String dbPassWord = "123456";   //数据库用户密码
+        String jdbcName = "com.mysql.jdbc.Driver";  //mysql驱动;
         Class.forName(jdbcName);
         Connection con = DriverManager.getConnection(dbUrl,dbUsername,dbPassWord);
         return con;
     }
 
     /**
-     * 关闭数据库连接
+     * @describe  增删资源的关闭
      * @param con
-     * @throws Exception
      */
-    public void closeCon(Connection con)throws Exception{
-        if(con != null)
-        {
-            con.close();
+    public static void closeRecourse(Connection con, PreparedStatement ps){
+        try{
+            if(ps != null)
+                ps.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            if(con != null)
+                con.close();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 
-/**
- * 这是数据库连接测试
+    /**
+     * @describe 查询资源的关闭
+     * @param con
+     * @param ps
+     * @param rs
+     */
+    public static void closeRecourse(Connection con, PreparedStatement ps, ResultSet rs){
+        try {
+            if(con != null)
+                con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
        public static void main(String[] args){
-        DbUtil dbUtil = new DbUtil();
         try{
-            Connection con = dbUtil.getCon();
+             DbUtil.getCon();
             System.out.println("数据库连接成功!");
             dbUtil.closeCon(con);
             System.out.println("数据库断开连接!");
+
+
         } catch (Exception e){
             e.printStackTrace();
             System.out.println("数据库连接失败!");
         }
     }
- */
+     */
 }
